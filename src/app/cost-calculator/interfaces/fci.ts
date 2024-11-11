7b20772f0789a781ba3e704fe5ebfc4b93d905fc
alanm <alanmedina1995@gmail.com>
2024-11-11 00:16:56 -0300

    Se agraga logica para calcular los rendimientos.

--- src/app/cost-calculator/interfaces/fci.ts
+++ src/app/cost-calculator/interfaces/fci.ts
@@ -1,7 +1,9 @@
+import { resultCalculateFci } from './result-calculate-fci';
 export interface FCI {
     fondo: string;
     tna: number;
     tea: number;
     tope: number | null;
     fecha: string;
+    resultCalculateFcis: { result: number, isWithdrawals: boolean }[];
 }
--- src/app/cost-calculator/pages/results-calculator-page/results-calculator.component.ts
+++ src/app/cost-calculator/pages/results-calculator-page/results-calculator.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CostCalculatorServiceService } from '../../services/cost-calculator-service.service';
 import { ResultCalcuflation } from '../../interfaces/result-calcuflation';
 import { FCI } from '../../interfaces/fci';
+import { resultCalculateFci } from '../../interfaces/result-calculate-fci';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results-calculator',
@@ -9,32 +11,68 @@ import { FCI } from '../../interfaces/fci';
 })
 export class ResultsCalculatorComponent {
 
-  public resultCalcuflation : ResultCalcuflation = {} as ResultCalcuflation;
-  private fci: FCI[] = [];
-
+  public resultCalcuflation: ResultCalcuflation = {} as ResultCalcuflation;
+  public fcis: FCI[] = [];
+  
   constructor(
-    private costCalculatorService: CostCalculatorServiceService
-  ){
-    
+    private costCalculatorService: CostCalculatorServiceService,
+    private route: Router
+  ) {
+
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getResultValues();
     this.getFciValus();
   }
 
-  getResultValues():void{
-      this.resultCalcuflation = this.costCalculatorService.resultCalcuflation;
+  getResultValues(): void {
+    this.resultCalcuflation = this.costCalculatorService.resultCalcuflation;
+
+    if (!this.resultCalcuflation || 
+      !this.resultCalcuflation.cashPrice) {
+      this.route.navigate(['./calculator/calculadora-simple']);
+    }
   }
 
   getFciValus(): void {
     this.costCalculatorService.getFciValues()
-    .subscribe((fciValues: FCI[]) => {
-      this.fci = fciValues;
-      console.log(this.fci);
-    });
+      .subscribe((fciValues: FCI[]) => {
+        this.fcis = fciValues;
+        this.calculateFinalAmountWithoutWithdrawals();
+        this.calculateFinalAmountWithWithdrawals();
+      });
   }
 
+  calculateFinalAmountWithoutWithdrawals(): void {
+    for (let fci of this.fcis) {
+      const dailyRate = fci.tna / 365;
+      const days = this.resultCalcuflation.installments * 30
+      let result = this.resultCalcuflation.cashPrice * Math.pow(1 + dailyRate, days);
+      if (!fci.resultCalculateFcis) {
+        fci.resultCalculateFcis = [];
+      }
+      fci.resultCalculateFcis.push({ result, isWithdrawals: false })
+    }
+  }
+
+  calculateFinalAmountWithWithdrawals(): void {
+
+    for (let fci of this.fcis) {
+      let balance = this.resultCalcuflation.cashPrice;
+      const dailyRate = fci.tna / 365;
+      for (let month = 1; month <= this.resultCalcuflation.installments; month++) {
+        for (let day = 1; day <= 30; day++) {
+          balance += balance * dailyRate;
+        }
+        balance -= this.resultCalcuflation.installmentValue;
+      }
+      if (!fci.resultCalculateFcis) {
+        fci.resultCalculateFcis = [];
+      }
+      fci.resultCalculateFcis.push({ result: balance, isWithdrawals: true })
+    }
+  }
 
 
 }
