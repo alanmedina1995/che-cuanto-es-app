ad575b0fa4575a1d3e5b65dce1e5e21573d1960e
Alan Medina <51724576+alanmedina1995@users.noreply.github.com>
2024-11-11 15:12:59 -0300

    Save inflationValue

--- src/app/cost-calculator/services/cost-calculator-service.service.ts
+++ src/app/cost-calculator/services/cost-calculator-service.service.ts
@@ -1,7 +1,7 @@
+import { InflationValue } from './../interfaces/inflation-value';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
-import { InflationValue } from '../interfaces/inflation-value';
+import { catchError, Observable, of, switchMap } from 'rxjs';
 import { FCI } from '../interfaces/fci';
 import { ResultCalcuflation } from '../interfaces/result-calcuflation';
 
@@ -11,6 +11,7 @@ import { ResultCalcuflation } from '../interfaces/result-calcuflation';
 export class CostCalculatorServiceService {
 
   private apiUrl: string = 'https://api.argentinadatos.com';
+  private dbUrl = 'http://localhost:3000';
 
   public resultCalcuflation: ResultCalcuflation = {} as ResultCalcuflation;
 
@@ -30,6 +31,36 @@ export class CostCalculatorServiceService {
       .pipe(catchError(() => of([])));
   }
 
+  saveDbInflationValue(inflationValue: InflationValue): Observable<InflationValue> {
+    return this.getDbInflationValue().pipe(
+      switchMap((inflationValueDb: InflationValue[]) => {
+        if (inflationValueDb.length > 0) {
+          const url = `${this.dbUrl}/inflation/1`;
+          return this.http.patch<InflationValue>(url, inflationValue).pipe(
+            catchError(error => {
+              console.error('Error update inflation value:', error);
+              return of({} as InflationValue);
+            })
+          );
+        } else {
+          const url = `${this.dbUrl}/inflation`;
+          return this.http.post<InflationValue>(url, inflationValue).pipe(
+            catchError(error => {
+              console.error('Error saving inflation value:', error);
+              return of({} as InflationValue);
+            })
+          );
+        }
+      })
+    );
+  }
+
+  getDbInflationValue(): Observable<InflationValue[]>{
+    const url = `${this.dbUrl}/inflation`;
+
+    return this.http.get<InflationValue[]>(url)
+      .pipe(catchError(() => of([])));
+  }
 
   setResultCalculator(resultCalcuflation: ResultCalcuflation): void {
     this.resultCalcuflation = resultCalcuflation;
