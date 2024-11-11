63db453376a1b1cfa2c71fdfbbbc10900ec64965
Alan Medina <51724576+alanmedina1995@users.noreply.github.com>
2024-11-11 17:23:15 -0300

    Fix visual

--- src/app/auth/services/auth.guard.ts
+++ src/app/auth/services/auth.guard.ts
@@ -12,12 +12,12 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): Observable<boolean> {
-    console.log("entre");
     return this.authService.auth().pipe(map((user) => {
       if (user) {
         return true;
       } else {
         this.router.navigate(['./auth/login']);
+        alert('Por favor inicia sesión.')
         return false;
       }
     }));
